d5d6cdf358c0d3933880c80482a7a2f657d8beaa
Ibrahim Khaled Reguieg <khaled.reguieg@gmail.com>
2015-12-13 22:06:52 +0100

    [CLIENT] ADD Client v1.2 filled requests and getting responses. Added two Threads for listening to for the responses and listening for console commands.

--- src/de/bht/vs/minimailbox/client/Client.java
+++ src/de/bht/vs/minimailbox/client/Client.java
@@ -1,10 +1,13 @@
 package de.bht.vs.minimailbox.client;
 
 import de.bht.vs.minimailbox.json.Request;
+import de.bht.vs.minimailbox.json.Response;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Calendar;
 
 /**
  * Created by Khaled on 30.11.2015.
@@ -12,7 +15,6 @@ import java.util.Calendar;
 public class Client {
 
     private static String input;
-    private static String jsonRequest;
     private static PrintWriter out;
     private static BufferedReader in;
     private static Request request;
@@ -26,74 +28,105 @@ public class Client {
         Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
         out = new PrintWriter(socket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        hilfe();
+        System.out.println("\n");
         System.out.print("$>");
-        while ((input = stdIn.readLine()) != null) {
-            if (input.equals("hilfe")) {
-                hilfe();
-                System.out.println("\n\n$>");
+        new Thread(() -> {
+            readConsole(stdIn);
+        }).start();
+
+        new Thread(() -> {
+            getResponse();
+        }).start();
+    }
+
+    private static void getResponse() {
+        Response response = null;
+        try {
+            while (true) {
+                response = Response.fromJson(in.readLine());
+                System.out.println(response.toJson() + "\n\n$>");
             }
-            if (input.startsWith("login")) {
-                login();
-                break;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readConsole(BufferedReader stdIn) {
+        try {
+            while ((input = stdIn.readLine()) != null) {
+                if (input.equals("hilfe")) {
+                    hilfe();
+                }
+                if (input.startsWith("login")) {
+                    login();
+                }
+                if (input.equals("time")) {
+                    time();
+                }
+                if (input.startsWith("ls")) {
+                    listDataInPath();
+                }
+                if (input.equals("who")) {
+                    listUsersInChat();
+                }
+                if (input.startsWith("msg")) {
+                    sendMsgAt();
+                }
+                if (input.equals("exit")) {
+                    sendExit();
+                }
             }
-            if (input.equals("time")) {
-                time();
-                break;
-            }
-            if (input.startsWith("ls")) {
-                listDataInPath();
-                break;
-            }
-            if (input.equals("who")) {
-                listUsersInChat();
-                break;
-            }
-            if (input.startsWith("msg")) {
-                sendMsgAt();
-                break;
-            }
-            if (input.equals("exit")) {
-                break;
-            }
-            System.out.print("$>");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private static void hilfe() {
-        System.out.println("hilfe\t\t -\tRuft die Hilfe auf.\n" +
+        System.out.println("hilfe\t\t\t\t\t -\tRuft die Hilfe auf.\n" +
                 "login <username>\t\t -\tLoggt den User ein.\n" +
-                "time\t -\tGibt die aktuelle Zeit aus.\n" +
-                "ls <Pfad>\t\t -\tGibt die Dateiliste des Pfades aus.\n" +
-                "who -\tGibt die Liste der verbundenen Clients aus.\n" +
-                "msg <Client> <message>\t\t -\tSendet Nachricht an <Client>\n" +
-                "exit\t\t -\tBeendet das Programm.");
+                "time\t\t\t\t\t -\tGibt die aktuelle Zeit aus.\n" +
+                "ls <Pfad>\t\t\t\t -\tGibt die Dateiliste des Pfades aus.\n" +
+                "who\t\t\t\t\t\t -\tGibt die Liste der verbundenen Clients aus.\n" +
+                "msg <Client> <message>\t -\tSendet Nachricht an <Client>\n" +
+                "exit\t\t\t\t\t -\tBeendet das Programm.");
     }
 
     public static void login() throws IOException {
         System.out.println("Logging in...");
         request = new Request(rndSequence, input.split(" ")[0], new String[]{input.split(" ")[1]});
         out.println(request.toJson());
-        //System.out.println("Login successful.");
         return;
     }
 
-    public static void time() {
-        // out.writeUTF();
-        System.out.println("Time: " + Calendar.getInstance().getTimeInMillis());
+    public static void time() throws IOException {
+        request = new Request(rndSequence, input, new String[]{});
+        out.println(request.toJson());
         return;
     }
 
     private static void listDataInPath() {
-
+        request = new Request(rndSequence, input.split(" ")[0], new String[]{input.split(" ")[1]});
+        out.println(request.toJson());
+        return;
     }
 
     private static void listUsersInChat() {
-
+        request = new Request(rndSequence, input, new String[]{});
+        out.println(request.toJson());
+        return;
     }
 
     private static void sendMsgAt() {
-
+        request = new Request(rndSequence, input.split(" ")[0], new String[]{input.split(" ")[1], input.split(" ")[2]});
+        out.println(request.toJson());
+        return;
     }
 
+    private static void sendExit() {
+        request = new Request(rndSequence, input, new String[]{});
+        out.println(request.toJson());
+        return;
+    }
 }
